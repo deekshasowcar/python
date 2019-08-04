29b4dd2df3d23ab2207789252b48da4225c6ddf1
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-05 01:36:05 +0530

    Create leap.py

---
+++ leap.py
@@ -0,0 +1,12 @@
+year=2000
+if(year%4)==0:
+ if(year%100)==0:
+  if(year%400)==0:
+   print("{0} is a leap year")
+  else:
+   print("{0} is not a leap year")
+  else:
+   print("{0} is a leap yaer")
+ else:
+ print("{0} is not a leap year")
+   
