eec29b98a006d750456fe818d9542d919c6abc66
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-04 21:43:15 +0530

    palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+def reverse(str):
+    return str[::-1]
+def ispalindrome:
+    rev=reverse(str)
+if(s==rev):
+    return true
+return false
+s="malayalam"
+ans=ispalindrome(str)
+if ans==1:
+    print("palindrome")
+else:
+    print("not a palindrome")
