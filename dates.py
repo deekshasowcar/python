9822da5273636be4cba137302f58e15b53f6ff53
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-06 13:40:26 +0530

    Update dates.py

--- dates.py
+++ dates.py
@@ -1,4 +1,4 @@
-a=int(input("enter the starting date")
-b=int(input("enter the ending date")
-for i in range(n1,n2):
-print(i)
+a=int(input("enter the starting date:"))
+b=int(input("enter the ending date:"))
+for i in range(a,b):
+ print(i)
