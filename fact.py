4339ed2536b1ac8e21fa31035c15d73b49089a47
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-06 13:56:06 +0530

    Update fact.py

--- fact.py
+++ fact.py
@@ -1,6 +1,9 @@
-def factorial(num):
-if num=1:
- return true
-else:
- return num*factorial(num-1)
 
+
+def factorial(num):
+ if num==1:
+  return True
+ else:
+  return num*factorial(num-1)
+num=int(input("input a number to compute the factorial:"))
+print(factorial(num))
