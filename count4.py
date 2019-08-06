5fd36a59c2ef16d36d9c2e08694c7e2f1921d684
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-06 13:36:04 +0530

    Update and rename count.py to count4.py

--- count4.py
+++ count4.py
@@ -1,8 +1,8 @@
 def list(a):
  count=0
- for a in nums:
-if a==4:
- count =count+1
-return count
+ for num in a:
+    if num==4:
+     count =count+1
+     return count
 print(list([1,2,3,4,5]))
 print(list([1,2,3,4,5,6]))
