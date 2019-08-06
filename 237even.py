449b95e7e940ddc8b935b19d4b6b126816894d30
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-06 12:19:56 +0530

    Update 237even.py

--- 237even.py
+++ 237even.py
@@ -1,6 +1,8 @@
-a=int(input(enter the number))
- if(a%2==0):
-  return a
-if(a>237):
-  break
-list([2,5,6,7,8,4,238])
+a=[24,48,236,237,28,42,462,326]
+for x in a:
+    if x==237:
+       print(x)
+       break;
+    elif x%2==0:
+         print(x)
+
