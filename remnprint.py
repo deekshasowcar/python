e33bfda5d9255cb7c35634cbfa2f62fb0662dda4
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-06 14:24:00 +0530

    Update remnprint.py

--- remnprint.py
+++ remnprint.py
@@ -1,10 +1,10 @@
 def remove(list):
    position=3-1
    index=0
-   len_list(len(list))
+   len_list=(len(list))
    while len_list>0:
      index=(position+index)%len_list
-     print(int_list.pop(index))
+     print(list.pop(index))
      len_list-=1
  nums=[10,20,30,40,70]
  remove(nums)
