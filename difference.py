ccd69aeb248630388b12e109952e766d597c81e0
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-06 13:46:35 +0530

    Update difference.py

--- difference.py
+++ difference.py
@@ -1,8 +1,8 @@
-def differnce(c)
-if n<=17:
- return 17-c
-else:
- return(n-17)*2
+def difference(c):
+ if c<=17:
+  return 17-c
+ else:
+  return(c-17)*2
 print(difference(33))
 print(difference(15))
 
