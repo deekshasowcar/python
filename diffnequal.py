822fd9309abc4a2ff556b34178f2b75d4e24e1bb
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-06 13:49:46 +0530

    Update diffnequal.py

--- diffnequal.py
+++ diffnequal.py
@@ -1,8 +1,8 @@
-a=int(input("enter the number"))
-b=int(input("enter the number"))
+a=int(input("enter the number:"))
+b=int(input("enter the number:"))
 c=a-b
 z=a+b
-if(a==b or c==d==5)
+if(a==b or c==z==5):
    print("true")
-else
+else:
    print("false")
