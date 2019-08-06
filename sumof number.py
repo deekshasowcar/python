003af01990050faa77f1780030abfa2a40a188c5
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-06 14:52:44 +0530

    Update sumof number.py

--- sumof number.py
+++ sumof number.py
@@ -1,6 +1,8 @@
-a=int(input("enter the first number"))
-b=int(input("enter the second number"))
-c=int(input("enter the third number"))
-z=a+b+c
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=int(input("enter the third number:"))
+sum=a+b+c
 if(a==b==c):
- y=3(z)
+ print(sum*3)
+else:
+ print(sum)
