04d90fef2ddc5371aa5c305cc786db1ddb310934
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-12 13:58:28 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,11 @@
+import array as arr
+a=[20,30,40]
+print("first element:",a[0])
+print("second element:",a[1])
+print("third element:",a[2])
+sum=(a[0]+a[1]+a[2])
+print(sum)
+avg=(a[0]+a[1]+a[2]/3)
+print(avg)
+   
+
