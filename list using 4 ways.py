5cb349722670afced66708a049918ab0e9d11ca1
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-12 14:54:28 +0530

    Add files via upload

---
+++ list using 4 ways.py
@@ -0,0 +1,20 @@
+a=[1,2,3,'a',4,5,6]
+for i in a:
+ print(i)
+ 
+list=['monday','tuesday','wednesday','thursday','friday']
+for i in list:
+ print(i)
+ 
+list_of_lists=[[1,2,3],['a','b','c']]
+list_of_lists.append([1,3,3])
+print(list_of_lists)
+
+
+list=list*2
+print(list)
+
+
+
+a.concatenate=[1,2,3]+[4]
+print(a)
