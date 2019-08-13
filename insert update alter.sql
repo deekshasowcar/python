38de680b08d240dec68ca6b6e61b38a6a3c8f631
deekshasowcar <53601056+deekshasowcar@users.noreply.github.com>
2019-08-13 14:31:18 +0530

    Create insert update alter.sql

---
+++ insert update alter.sql
@@ -0,0 +1,8 @@
+--SELECT * FROM demo;
+--CREATE table gundu133312('name','regno','age');
+--insert into gundu133312('name','regno','age') VALUES('dee',233,31);
+--select * from gundu133312;
+--update gundu133312 set name="deeksha" where regno=233;
+--select*from gundu133312;
+--alter table gundu133312 add COLUMN email varchar(20); 
+select* from gundu133312;
